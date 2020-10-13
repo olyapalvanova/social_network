e640a0ae1b27549daabdf340eac105d4f0eb1277
olga <olya_palva@bk.ru>
2020-10-13 11:29:13 +0300

    Minor refactoring, bug fix.

--- src/apps/dashboard/forms.py
+++ src/apps/dashboard/forms.py
@@ -4,19 +4,7 @@ from django.core.exceptions import ValidationError
 from apps.users.models import User
 
 
-class UserForm(forms.Form):
-    first_name = forms.CharField(label='Your name', max_length=50)
-    last_name = forms.CharField(label='Your last name', max_length=50)
-    birth_date = forms.DateField(required=False)
-    city = forms.CharField(label='Your city', max_length=50,
-                           required=False)
-    education = forms.CharField(label='Your education', max_length=50,
-                                required=False)
-    job = forms.CharField(label='Your job', max_length=50,
-                          required=False)
-
-
-class UserInfoForm(forms.ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'birth_date', 'city', 'school',
